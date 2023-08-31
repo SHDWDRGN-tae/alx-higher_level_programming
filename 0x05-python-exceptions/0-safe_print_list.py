4e53751d1e784e264d12393c908d3225e4d4fa07
SHDWDRGN-tae <taepeolwane@gmail.com>
2023-08-30 20:00:26 -0700

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i is not x:
-            print(my_list[i[, ends='')
+            print(my_list[i], end='')
             i += 1
     except IndexError:
-    None
+        None
     print()
     return i
